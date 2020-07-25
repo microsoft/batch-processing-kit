3cb696e274e7b8050437499fd6b0f81f72e3c27f
Andrew Wald <andwald@microsoft.com>
2020-07-25 05:05:33 -0700

    Move pygoject dep as an examples dep since not needed in base framework. Smaller foolprint and fast pip install of base framework. (#8)

--- setup_batchkit.py
+++ setup_batchkit.py
@@ -31,7 +31,7 @@ for line in required:
 # Package specification for batchkit library.
 setup(
     name='batchkit',
-    version='0.9.2-dev1',
+    version='0.9.2-dev2',
     author='Microsoft Azure',
     author_email='andwald@microsoft.com',
     description="Generic batch processing framework for managing the orchestration, dispatch, fault tolerance, and "
